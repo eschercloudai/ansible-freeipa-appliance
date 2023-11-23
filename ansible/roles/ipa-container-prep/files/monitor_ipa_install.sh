35ee4f794320d2a7ac4fed3cd4ccac5b6c68053e
antonycleave <a.cleave@eschercloud.ai>
2023-11-23 11:19:16 +0000

    first working merge (#1)
    *  passing tests now
    
    * speed up by adding Setup Complete to good exits'
    
    * restructured playbook for safer operations
    
    * move ipa hostname and fqdn vars into all
    
    * add checks to make sure that ipa_container_realm  and ipa_container_domain are not null
    
    * add role to template resolv.conf on ipaservers
    
    * enable resolv.conf templating and change group_vars defaults to accept an ipa_cluster_suffix which gets uppered and lowered for the Real and fqdn accordingly
    
    * fixes to allow populating resolv.conf on the ipa vms with localhost, the list of othe servers and an optional extra list
    
    * fix single node clusters and issues on 1st run
    
    ---------
    
    Co-authored-by: Antony Cleave <a.cleave@eshercloud.ai>

---
+++ ansible/roles/ipa-container-prep/files/monitor_ipa_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if podman inspect "$1" | grep --q '"Running": true'; then
+  if podman logs "$1" | grep  -e "FreeIPA server configured." -e "Setup complete" -e "FreeIPA server started"; then
+    exit 0
+  else
+    echo not there yet
+    exit 2
+  fi
+else
+  echo >&2 "container with uuid/name $1 is not running stop waiting and fail"
+  exit 1
+fi
